918f6a12f6f855a37c8b7f80b3038cd94d41d926
wogks <wogks@wogksui-MacBookPro.local>
2022-09-27 17:51:41 +0900

    Feat: stock repo impl 수정

--- lib/data/repository/stock_repository_impl.dart
+++ lib/data/repository/stock_repository_impl.dart
@@ -1,4 +1,5 @@
 //인터페이스의 기능들을 구현
+import 'package:stock_app/data/csv/company_listings_parser.dart';
 import 'package:stock_app/data/mapper/company_mapper.dart';
 import 'package:stock_app/data/source/remote/stock_api.dart';
 import 'package:stock_app/domain/model/company_listing.dart';
@@ -10,6 +11,7 @@ import '../source/local/stock_dao.dart';
 class StockRepositoryImpl implements StockRepository {
   final StockApi _api; //리모트해서 데이터를 가져올때 필요함
   final StockDao _dao; //로컬에 접근할수있는 다오
+  final _parser = CompanyListingsParser();
   StockRepositoryImpl(this._api, this._dao);
 
   @override
@@ -30,11 +32,21 @@ class StockRepositoryImpl implements StockRepository {
     }
     //리모트
     try {
-      final remoteListings= await _api.getListings();
-      return Result.success([]);
+      final response= await _api.getListings();
+      final remoteListings = await _parser.parse(response.body);
+
+      //캐시 비우기(안그러면 계속 데이터가 추가가 됨)
+      await _dao.clearCompanyListings();
+
+
+      //데이터를 받아오면 캐싱을 해야함. db에 넣는로직(캐싱 추가)
+      await _dao.insertCompanyListings(
+        remoteListings.map((e) => e.toCompanyListingEntity()).toList()
+      );
+      return Result.success(remoteListings);
     } catch (e) {
       return Result.error(Exception('데이터로드 실패'));
     }
-    throw UnimplementedError();
+    
   }
 }
